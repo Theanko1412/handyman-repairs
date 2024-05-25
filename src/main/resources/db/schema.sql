42c6993c1c299f4babf0a134aaca8654594af074
Theanko <danko.curlin@hotmail.com>
2024-05-25 14:08:05 +0200

    schema update

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,95 +1,96 @@
---may be older version
-
 create table country (
-  country_id character varying(255) primary key not null,
-  name character varying(255)
+  country_id varchar(255) primary key not null,
+  name varchar(255)
 );
 
 create table city (
-  country_id character varying(255) not null,
-  name character varying(255),
-  town_id character varying(255) primary key not null,
+  town_id varchar(255) primary key not null,
+  name varchar(255),
+  country_id varchar(255) not null,
   foreign key (country_id) references country (country_id)
 );
 
 create table street (
-  city_id character varying(255) not null,
-  name character varying(255),
+  street_id varchar(255) primary key not null,
+  name varchar(255),
   number integer,
-  street_id character varying(255) primary key not null,
+  city_id varchar(255) not null,
   foreign key (city_id) references city (town_id)
 );
 
 create table home_or_workshop (
-  home_or_workshop_id character varying(255) primary key not null,
-  street_id character varying(255) not null,
+  homeOrWorkshop_id varchar(255) primary key not null,
+  name varchar(255),
+  street_id varchar(255) not null,
   foreign key (street_id) references street (street_id)
 );
 
 create table category (
-  category_id character varying(255) primary key not null,
-  description character varying(255),
-  name character varying(255)
+  category_id varchar(255) primary key not null,
+  name varchar(255),
+  description varchar(255)
+);
+
+create table handyman (
+  handyman_id varchar(255) primary key not null,
+  first_name varchar(255),
+  last_name varchar(255),
+  email varchar(255),
+  password varchar(255),
+  rating double precision,
+  is_suspended boolean,
+  homeOrWorkshop_id varchar(255),
+  foreign key (homeOrWorkshop_id) references home_or_workshop (homeOrWorkshop_id)
 );
 
 create table service (
-  duration integer,
+  service_id varchar(255) primary key not null,
+  name varchar(255),
+  description varchar(255),
   price double precision,
-  category_id character varying(255) not null,
-  description character varying(255),
-  name character varying(255),
-  service_id character varying(255) primary key not null,
-  handyman_id character varying(255),
+  duration integer,
+  category_id varchar(255) not null,
+  handyman_id varchar(255) not null,
   foreign key (category_id) references category (category_id),
   foreign key (handyman_id) references handyman (handyman_id)
 );
 
 create table customer (
-  is_suspended boolean,
+  customer_id varchar(255) primary key not null,
+  first_name varchar(255),
+  last_name varchar(255),
+  email varchar(255),
+  password varchar(255),
   strikes integer,
-  customer_id character varying(255) primary key not null,
-  email character varying(255),
-  first_name character varying(255),
-  home_or_workshop_id character varying(255),
-  last_name character varying(255),
-  foreign key (home_or_workshop_id) references home_or_workshop (home_or_workshop_id)
-);
-
-create table handyman (
   is_suspended boolean,
-  rating double precision,
-  email character varying(255),
-  first_name character varying(255),
-  handyman_id character varying(255) primary key not null,
-  home_or_workshop_id character varying(255),
-  last_name character varying(255),
-  foreign key (home_or_workshop_id) references home_or_workshop (home_or_workshop_id)
+  homeOrWorkshop_id varchar(255),
+  foreign key (homeOrWorkshop_id) references home_or_workshop (homeOrWorkshop_id)
 );
 
 create table notification (
-  date timestamp(6) with time zone,
-  customer_id character varying(255),
-  handyman_id character varying(255),
-  message character varying(255),
-  notification_id character varying(255) primary key not null,
-  sender character varying(255),
+  notification_id varchar(255) primary key not null,
+  message varchar(255),
+  date timestamp with time zone,
+  sender varchar(255),
+  customer_id varchar(255),
+  handyman_id varchar(255),
   foreign key (customer_id) references customer (customer_id),
   foreign key (handyman_id) references handyman (handyman_id)
 );
 
 create table schedule (
-  handyman_id character varying(255),
-  schedule_id character varying(255) primary key not null,
+  schedule_id varchar(255) primary key not null,
+  handyman_id varchar(255),
   foreign key (handyman_id) references handyman (handyman_id)
 );
 
 create table reservation (
-  date timestamp(6) with time zone,
-  id character varying(255) primary key not null,
-  schedule_id character varying(255) not null,
-  service_id character varying(255) not null,
-  customer_id character varying(255) not null,
-  status character varying(255) NOT NULL,
+  id varchar(255) primary key not null,
+  date timestamp with time zone,
+  status varchar(255) not null,
+  customer_id varchar(255) not null,
+  schedule_id varchar(255) not null,
+  service_id varchar(255) not null,
   foreign key (customer_id) references customer (customer_id),
   foreign key (service_id) references service (service_id),
   foreign key (schedule_id) references schedule (schedule_id)
